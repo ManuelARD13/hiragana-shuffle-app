4ee2738debc4f4428c4f15d5bedd646c9739024a
swlman <duranalejandro661@gmail.com>
2024-01-27 20:19:04 -0300

    Initializing typescript base setup for React with Sass.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./sass/main.scss"
+
+function App(): JSX.Element {
+ 
+
+  return (
+    <div>
+      Hello typescript
+    </div>
+  );
+}
+
+export default App;
